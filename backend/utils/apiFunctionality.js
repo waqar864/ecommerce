0ec5eeb42e3dabe3f9cfede181f4d1f24ea7655b
waqar864 <waqarkhan864@gmail.com>
2025-09-10 19:56:36 +0500

    paginations

--- backend/utils/apiFunctionality.js
+++ backend/utils/apiFunctionality.js
@@ -13,6 +13,17 @@ class APIFunctionality{
         this.query = this.query.find({...keyword});
         return this;
     }
+    filter(){
+        const queryCopy = {...this.queryStr};
+        //Removing fields for category
+        const removeFields = ["keyword","page","limit"];
+        removeFields.forEach((key) => delete queryCopy[key]);
+        //Filter for price and rating
+        let queryStr = JSON.stringify(queryCopy);
+        queryStr = queryStr.replace(/\b(gt|gte|lt|lte)\b/g, (key) => `$${key}`);
+        this.query = this.query.find(JSON.parse(queryStr));
+        return this;
+    }
 }
 
 export default APIFunctionality
